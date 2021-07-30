fb16bfa935dc6ff8d0f043f7757d0f13679b5a44
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-07-30 12:05:39 -0300

    Endpoint /clientes{id} disponivel

---
+++ src/main/java/com/cursomc/resources/ClienteResource.java
@@ -0,0 +1,35 @@
+package com.cursomc.resources;
+
+import ch.qos.logback.core.net.server.Client;
+import com.cursomc.domain.Categoria;
+import com.cursomc.domain.Cliente;
+import com.cursomc.services.CategoriaService;
+import com.cursomc.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+
+        /*
+        ResponseEntity é um tipo especial do Spring que já encapsula várias informações
+            de uma resposta HTTP para um serviço Rest.
+         */
+
+        Cliente obj = service.buscar(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+}
---
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.cursomc.services;
+
+import com.cursomc.domain.Cliente;
+import com.cursomc.repositories.ClienteRepository;
+import com.cursomc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired //instancia o objeto automaticamente
+    private ClienteRepository repo;
+
+    public Cliente buscar(Integer id) {
+        Optional<Cliente> obj = repo.findById(id);
+
+        return obj.orElseThrow( () -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));    //Lança uma exceção caso o Id não exista
+    }
+}
