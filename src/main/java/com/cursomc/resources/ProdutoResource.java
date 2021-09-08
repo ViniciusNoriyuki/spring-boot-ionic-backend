31dfa94ab9eac57280ffd928f94b2ded7e063a42
Vinicius Noriyuki <vinicius_noriyuki@hotmail.com>
2021-09-08 13:20:21 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/cursomc/resources/ProdutoResource.java
@@ -0,0 +1,46 @@
+package com.cursomc.resources;
+
+import com.cursomc.domain.Categoria;
+import com.cursomc.domain.Produto;
+import com.cursomc.dto.CategoriaDTO;
+import com.cursomc.dto.ProdutoDTO;
+import com.cursomc.resources.utils.URL;
+import com.cursomc.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+
+    @Autowired
+    private ProdutoService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Produto> find(@PathVariable Integer id) {
+        Produto obj = service.find(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Page<ProdutoDTO>> findPage(
+            @RequestParam(value = "nome", defaultValue = "0") String nome,
+            @RequestParam(value = "categorias", defaultValue = "0") String categorias,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+        String nomeDecoded = URL.decodeParam(nome);
+        List<Integer> ids = URL.decodeIntList(categorias);
+        Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+        Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
+
+        return ResponseEntity.ok().body(listDto);
+    }
+}
